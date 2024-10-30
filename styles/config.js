7db0dcf895ec57a9e9e17133dd4c5320f03a8bc8
sisukalat <44116791+sisukalat@users.noreply.github.com>
2024-10-30 16:58:19 -0400

    changes adding filter faq

--- styles/config.js
+++ styles/config.js
@@ -92,7 +92,7 @@ let options = {
         logSheetPage: sheetPages.inventoryLog,
     
         itemAmount: 24,
-        sortTypes: ['Currency', 'MYO Slot', 'Standard Trait', 'Nature Trait', 'Alteration', 'Breeding', 'Pets'],
+        sortTypes: ['Currency', 'MYO Slot', 'Standard Trait', 'Nature Trait', 'Alteration', 'Breeding', 'Pets',],
         
         searchFilterParams: ['Username'],
     
@@ -162,9 +162,10 @@ let options = {
     
         itemAmount: 24,
         itemOrder: "asc",
-    
-        sortTypes: 'Category',
+
+        filterColumn: 'Category',
         searchFilterParams: ['Tags'],
+        
     
     },
 
